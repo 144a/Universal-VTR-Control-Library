781eb749fdb898250e0d1f76dcc70a8f31461aac
Andy Gatza <andy.gatza@gmail.com>
2024-07-20 20:48:52 -0500

    Added basic command list

--- uvcl/commands.py
+++ uvcl/commands.py
@@ -4,7 +4,7 @@
 #| CMD1 / Data Count |       CMD 2       |  Data n (n=0~15)  |      Checksum     |
 #|  MSD      LSD     |                   |                   |                   |
 #|-------------------|-------------------|-------------------|-------------------|
-#       1 Byte              1 Byte              1 Byte              1 Byte       
+#       1 Byte              1 Byte             1-n Bytes             1 Byte       
 # Checksum 
 # CMD 1 
 # Determines the function/type of command being sent/recieved
@@ -17,6 +17,9 @@ CMD1 = {
     'SENSE RETURN' : 7, # From VTR
 }
 
+# Command table is from the DVR-2000/2100 documentation
 CMD2 = {
-    
+    'Local Disable' : ['00 0c', 'Ack'],
+    'Device Type Request' : ['12 11', 'Device Type'],
+    'Local Enable' : ['00 1D', 'Ack']
 }
